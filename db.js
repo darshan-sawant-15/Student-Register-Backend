5cf10d24f0a9b44bd5fd34d16bf24b8e3293e6cc
darshan-sawant-15 <darshansawant285@gmail.com>
2023-10-22 19:42:36 +0530

    db change

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/studentdb");
+// mongoose.connect("mongodb://127.0.0.1:27017/studentdb");
+mongoose.connect("mongodb+srv://darshansawant285:5Zg6S7O1PrnJ5U9u@cluster0.9ssvc5m.mongodb.net/studentdb");
 
 const conn = mongoose.connection;
 
